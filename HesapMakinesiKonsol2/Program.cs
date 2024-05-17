ef0a353b632e14ddb20b58b4f22a88e9ddb53f36
tahsindincel <dnclthsn@gmail.com>
2024-05-17 19:07:39 +0300

    Değişiklik
    Try Catch ekledim

--- HesapMakinesiKonsol2/Program.cs
+++ HesapMakinesiKonsol2/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,61 +10,68 @@ namespace HesapMakinesiKonsol2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hesap Makinesi- Değerleri ve İşaretleri giriniz. Çıkmak için ESC basınız.");
-            bool esc = false;
-            Console.Write("  Sayı giriniz:");
-            double a = Convert.ToDouble(Console.ReadLine());
-            Console.Write("Toplama: + \nÇıkarma: - \nÇarpma: * \nBölme: / \n İşlem Seçin:");
-            char chr = Convert.ToChar(Console.ReadLine());
-            Console.Write("  Sayı giriniz:");
-            double b = Convert.ToDouble(Console.ReadLine());
-            Console.Write("Sonuç:= \nToplama: + \nÇıkarma: - \nÇarpma: * \nBölme: / \nÇıkış:E \n İşlem Seçin:");
-            char chr2 = Convert.ToChar(Console.ReadLine());
-            double c = 0;
-            double f = 0;
-            while (esc == false)
+            try 
             {
-                if (chr2 == '=')
+                Console.WriteLine("Hesap Makinesi- Değerleri ve İşaretleri giriniz. Çıkmak için ESC basınız.");
+                bool esc = false;
+                Console.Write("  Sayı giriniz:");
+                double a = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Toplama: + \nÇıkarma: - \nÇarpma: * \nBölme: / \n İşlem Seçin:");
+                char chr = Convert.ToChar(Console.ReadLine());
+                Console.Write("  Sayı giriniz:");
+                double b = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Sonuç:= \nToplama: + \nÇıkarma: - \nÇarpma: * \nBölme: / \nÇıkış:E \n İşlem Seçin:");
+                char chr2 = Convert.ToChar(Console.ReadLine());
+                double c = 0;
+                double f = 0;
+                while (esc == false)
                 {
-                    if (chr == '+')
-                        f = a + b;
-                    else if (chr == '-')
-                        f = a - b;
-                    else if (chr == '*')
-                        f = a * b;
-                    else if (chr == '/')
-                    {
-                        if (b == 0)
-                            Console.WriteLine("Sıfıra Bölme İşlemi Tanımsızdır");
-                        else
-                            f = a / b;
-                    }
-                    else
-                        Console.WriteLine("Bu Makine Sadece 4 işlem yapabilmektedir");
-                    Console.Write(f);
-                    esc = true;
-                    Console.ReadKey();
-
-                }
-                else if(chr2 == 'E')
-                {
-                    esc =true;
-
-
-                }
-                else
-                {
-                    c = Hesaplama.Hesapla(a, b, chr, chr2);
-                    //Console.Write("Sonuç: " + c);
-                    b = c;
-                    Console.Write("Sonuç:= \nToplama: + \nÇıkarma: - \nÇarpma: * \nBölme: / \nÇıkış:E \n İşlem Seçin:");
-                    chr2 = Convert.ToChar(Console.ReadLine());
                     if (chr2 == '=')
                     {
-                        Console.Write("Sonuç: " + c);
+                        if (chr == '+')
+                            f = a + b;
+                        else if (chr == '-')
+                            f = a - b;
+                        else if (chr == '*')
+                            f = a * b;
+                        else if (chr == '/')
+                        {
+                            if (b == 0)
+                                Console.WriteLine("Sıfıra Bölme İşlemi Tanımsızdır");
+                            else
+                                f = a / b;
+                        }
+                        else
+                            Console.WriteLine("Bu Makine Sadece 4 işlem yapabilmektedir");
+                        Console.Write(f);
+                        esc = true;
+                        Console.ReadKey();
+
+                    }
+                    else if (chr2 == 'E')
+                    {
+                        esc = true;
+
+
+                    }
+                    else
+                    {
+                        c = Hesaplama.Hesapla(a, b, chr, chr2);
+                        b = c;
+                        Console.Write("Sonuç:= \nToplama: + \nÇıkarma: - \nÇarpma: * \nBölme: / \nÇıkış:E \n İşlem Seçin:");
+                        chr2 = Convert.ToChar(Console.ReadLine());
+                        if (chr2 == '=')
+                        {
+                            Console.Write("Sonuç: " + c);
+                        }
                     }
                 }
             }
+            catch (FormatException)
+            {
+                Console.Write("Doğru değerler gir");
+            }
+            Console.ReadKey();
         }
     }
     
